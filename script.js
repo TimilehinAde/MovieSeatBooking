c7af760eb2f4b92532f3eb5a153879ba8d4d01b7
timi <adeloduntimilehin15@gmail.com>
2023-05-31 18:15:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+populateUI();
+
+let ticketPrice = +movieSelect.value;
+
+function setMovieData(movieIndex, moviePrice){
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+//update total and count
+ function updateSelectedCount(){
+  const selectedSeats = document.querySelectorAll('.row.seat.selected');
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
+   const selectedSeatsCount = selectedSeats.length;
+
+   console.log(selectedSeatsCount);
+
+   count.innerText = selectedSeatsCount;
+   total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//Get data from localstorage and populate UI
+function populateUI(){
+  console.log(localStorage.getItem('selectedSeats'));
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  console.log(selectedSeats);
+}
+
+//Movie select event
+movieSelect.addEventListener('change', (e) =>{
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.value, e.target.value);
+  updateSelectedCount;
+});
+
+//Seat click event
+container.addEventListener('click', (e) => {
+  if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
